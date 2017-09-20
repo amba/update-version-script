eeb5c520ff1a741927edbe359174d2d4829f7650
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2017-09-20 15:59:16 +0200

    Add Getopt stuff.

--- update-version.pl
+++ update-version.pl
@@ -7,30 +7,66 @@ use JSON::XS;
 use File::Slurper 'read_binary';
 use File::Find;
 use autodie qw/:all/;
+use Getopt::Long;
 
-system(qw/perl Makefile.PL/);
+verbose_system(qw/perl Makefile.PL/);
 
 my $meta_json = decode_json(read_binary('MYMETA.json'));
+
+my $new_version;
+my $dry_run;
+my $print_help;
+
+GetOptions(
+    "version|v=s" => \$new_version,
+    "dry-run|d"   => \$dry_run,
+    "help|h"      => \$print_help,
+    )
+    or die "GetOptions";
+
+if ($print_help) {
+    print <<"EOF";
+update-version [OPTIONS]
+
+Options:
+  -v, --version=VERSION             Set new version to VERSION. Default is
+                                    to increment the last version.
+  -d, --dry-run                     Do nothing. Only print status messages.
+  -h, --help                        Print this help.
+EOF
+    exit;
+}
+
+if ($dry_run) {
+    say "Doing dry run";
+}
+
 my $version = $meta_json->{version};
 
 if ($version !~ /^(?<major>[0-9])\.(?<minor>[0-9]{2})$/) {
     die "version $version is invalid";
 }
 
+
 my $major = $+{major};
 my $minor = $+{minor};
 
 printf("current version number: %d.%02d\n", $major, $minor);
 
-if ($minor == 99) {
-    $minor = 0;
-    ++$major;
-}
-else {
-    ++$minor;
+if (not defined $new_version) {
+    # increment version
+    if ($minor == 99) {
+        $minor = 0;
+        ++$major;
+    }
+    else {
+        ++$minor;
+    }
+
+    $new_version = sprintf("%d.%02d", $major, $minor);
 }
 
-my $new_version = sprintf("%d.%02d", $major, $minor);
+
 say "new version number: $new_version";
 
 # Replace 'our $VERSION = ...' in .pm files
@@ -43,18 +79,24 @@ for my $module (find_modules()) {
 
 
 # Git tag & commit
-system('git', 'commit', '-am', "update version $version -> $new_version");
-system('git', 'tag', '-a', "v$new_version", '-m', "version $new_version");
+verbose_system('git', 'commit', '-am', "update version $version -> $new_version");
+verbose_system('git', 'tag', '-a', "v$new_version", '-m', "version $new_version");
+
+verbose_system(qw/git push/);
+verbose_system(qw/git push --tags/);
 
 # Rerun Makefile.PL to get the new version
-system(qw/perl Makefile.PL/);
+verbose_system(qw/perl Makefile.PL/);
 
 
 
 sub write_file ($filename, $content) {
-    open my $fh, ">", $filename;
-    print {$fh} $content;
-    close $fh;
+    say "updating file $filename";
+    if (not $dry_run) {
+        open my $fh, ">", $filename;
+        print {$fh} $content;
+        close $fh;
+    }
 }
 
 sub find_modules {
@@ -68,4 +110,12 @@ sub find_modules {
         );
     say "modules: @files";
     return @files;
+}
+
+
+sub verbose_system(@command) {
+    say "running command: @command";
+    if (not $dry_run) {
+        system(@command);
+    }
 }
